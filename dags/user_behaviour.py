29626fc625fdad46fdc753d7f418c50abd772be5
Scott-Huston <scottphuston@gmail.com>
2020-10-27 18:18:21 -0700

    Added filler code for connecting to s3

--- dags/user_behaviour.py
+++ dags/user_behaviour.py
@@ -3,11 +3,26 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.operators import PythonOperator
 
 # config
 # local
 unload_user_purchase ='./scripts/sql/filter_unload_user_purchase.sql'
 temp_filtered_user_purchase = '/temp/temp_filtered_user_purchase.csv'
+
+# remote config
+BUCKET_NAME = '<your-bucket-name>'
+temp_filtered_user_purchase_key= 'user_purchase/stage/{{ ds }}/temp_filtered_user_purchase.csv'
+
+# helper function(s)
+def _local_to_s3(filename, key, bucket_name=BUCKET_NAME):
+    s3 = S3Hook()
+    s3.load_file(filename=filename, bucket_name=bucket_name,
+                 replace=True, key=key)
+
+# Ideally I'd have configs in a different file or set them as docker env variables, 
+# but due to this being a simple example I'll keep them with the DAG script.
 
 default_args = {
     "owner": "airflow",
@@ -36,4 +51,14 @@ pg_unload = PostgresOperator(
     wait_for_downstream=True
 )
 
-pg_unload >> end_of_data_pipeline
+user_purchase_to_s3_stage = PythonOperator(
+    dag=dag,
+    task_id='user_purchase_to_s3_stage',
+    python_callable=_local_to_s3,
+    op_kwargs={
+        'filename': temp_filtered_user_purchase,
+        'key': temp_filtered_user_purchase_key,
+    },
+)
+
+pg_unload >> user_purchase_to_s3_stage >> end_of_data_pipeline
