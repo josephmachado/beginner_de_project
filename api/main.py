d8545b193979b775cfba507414f3b31d03ae889f
Alan Huang <alanhuang@binflux.com>
2023-03-16 16:57:48 +0800

    feat: recommend product api

---
+++ api/main.py
@@ -0,0 +1,29 @@
+import os
+from fastapi import FastAPI
+from models import RecommendRequest
+
+from elasticsearch import Elasticsearch
+from utils import get_similar
+
+ES_HOST = os.env['ES_HOST']
+ES_PORT = os.env['ES_PORT']
+app = FastAPI()
+
+def get_es():
+    endpoint = f'http://{ES_HOST}:{ES_PORT}'
+    es = Elasticsearch(endpoint)
+    return es
+
+@app.post("/recommend_product")
+def recommend_product(recommend_request: RecommendRequest):
+    es = get_es()
+    product, recs = get_similar(es, recommend_request.id, num=recommend_request.rec_num, index='products')
+    recommendations = []
+    for rec in recs:
+        r = {}
+        r['asin'] = rec['_source']['asin']
+        r['title'] = rec['_source']['title']
+        r['score'] = rec['_score']
+        recommendations.append(r)
+    return recommendations
+
