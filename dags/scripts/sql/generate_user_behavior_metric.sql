1a1be25893c1f3e84be9be7301b731a11829a639
JosephKevinMachado <josephkevinmachado@pop-os.fios-router.home>
2024-06-12 17:05:27 -0400

    2024-06-12-17-05-26

--- dags/scripts/sql/generate_user_behavior_metric.sql
+++
@@ -1,7 +0,0 @@
-with up as ( select * from '/opt/airflow/temp/s3folder/raw/user
-_purchase/user_purchase.csv'), mr as (select * from '/opt/airflow/t
-emp/s3folder/clean/movie_review/*.parquet') select up.customer_id, 
-sum(up.quantity * up.unit_price) as amount_spent, sum(case when mr.
-positive_review then 1 else 0 end) as num_positive_reviews, count(m
-r.cid) as num_reviews from up join mr on up.customer_id = mr.cid gr
-oup by up.customer_id;
